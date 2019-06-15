a07fbbf20721617dfa723c8917f68afe45658cfe
Pablo <pgarcgo@bloque64.com>
2019-06-15 23:41:24 +0200

    Commiting

---
+++ bloque_vue/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Portada</h1>
+    <p>Articulos de Portada</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home' //this is the name of the component
+}
+</script>
+<style>
+</style>
---
+++ bloque_vue/src/routes.js
@@ -0,0 +1,13 @@
+// routes.js
+
+import Home from './components/Home.vue';
+import Register from './components/Register.vue';
+import Login from './components/Login.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/register', component: Register },
+    { path: '/login', component: Login },
+];
+
+export default routes;
