039cc888a58afdc86c42e3fade778a304b91af39
Pablo <pgarcgo@bloque64.com>
2019-06-17 08:15:28 +0200

    Advancing

---
+++ node_modules/vue-navigation-bar/src/components/BrandImage.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="vnb__brand-image-wrapper">
+        <dynamic-link
+            :path="options.brandImagePath"
+            :isUsingVueRouter="options.isUsingVueRouter"
+            class="vnb__brand-image-wrapper__link"
+            aria-label="Homepage"
+        >
+            <img
+                v-if="options.brandImage"
+                :src="options.brandImage"
+                :alt="options.brandImageAltText"
+                class="vnb-image vnb__brand-image-wrapper__link__image"
+            >
+        </dynamic-link>
+    </div>
+</template>
+
+<script>
+import DynamicLink from '../components/DynamicLink.vue'
+
+export default {
+    name: 'brand-image',
+    props: {
+        options: {
+            type: Object,
+            required: true
+        }
+    },
+    data () {
+        return {
+        }
+    },
+    computed: {
+    },
+    methods: {
+    },
+    components: {
+        DynamicLink
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '../assets/css/main.scss';
+
+    .vnb {
+        &__brand-image-wrapper {
+            padding-left: 10px;
+
+            &__link {
+                &__image {
+                    max-height: 34px;
+                }
+            }
+        }
+    }
+
+    .vnb-image {
+        max-width: 100%;
+        height: auto;
+    }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,108 @@
+<template>
+		<footer class="footer-basic-centered">
+
+			<p class="footer-company-motto">The company motto.</p>
+
+			<p class="footer-links">
+				<a href="#">Home</a>
+				·
+				<a href="#">Blog</a>
+				·
+				<a href="#">Pricing</a>
+				·
+				<a href="#">About</a>
+				·
+				<a href="#">Faq</a>
+				·
+				<a href="#">Contact</a>
+			</p>
+
+			<p class="footer-company-name">Company Name &copy; 2015</p>
+
+		</footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.footer-basic-centered{
+	background-color: #292c2f;
+	box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+	box-sizing: border-box;
+	width: 100%;
+	text-align: center;
+	font: normal 18px sans-serif;
+
+	padding: 45px;
+	margin-top: 80px;
+}
+
+.footer-basic-centered .footer-company-motto{
+	color:  #8d9093;
+	font-size: 24px;
+	margin: 0;
+}
+
+.footer-basic-centered .footer-company-name{
+	color:  #8f9296;
+	font-size: 14px;
+	margin: 0;
+}
+
+.footer-basic-centered .footer-links{
+	list-style: none;
+	font-weight: bold;
+	color:  #ffffff;
+	padding: 35px 0 23px;
+	margin: 0;
+}
+
+.footer-basic-centered .footer-links a{
+	display:inline-block;
+	text-decoration: none;
+	color: inherit;
+}
+
+/* If you don't want the footer to be responsive, remove these media queries */
+
+@media (max-width: 600px) {
+
+	.footer-basic-centered{
+		padding: 35px;
+	}
+
+	.footer-basic-centered .footer-company-motto{
+		font-size: 18px;
+	}
+
+	.footer-basic-centered .footer-company-name{
+		font-size: 12px;
+	}
+
+	.footer-basic-centered .footer-links{
+		font-size: 14px;
+		padding: 25px 0 20px;
+	}
+
+	.footer-basic-centered .footer-links a{
+		line-height: 1.8;
+	}
+}
+
+</style>
+
+
+
