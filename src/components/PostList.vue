42ae187cb2738bc57dfa1435b2a9704f0495ea0b
pgarcgo <pgarciagon@gmail.com>
2019-07-15 17:07:25 +0200

    avancing

---
+++ src/components/PostList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ category }}</h1>
+    <p>...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList', //this is the name of the component,
+  props: ["category", "sort_by"]
+}
+</script>
+<style>
+</style>
