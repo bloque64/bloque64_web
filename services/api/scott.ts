f2cefa8180ca844f87cdff7f9b09441f7dbc639e
DiazRock <corolariodiaz@gmail.com>
2020-03-09 01:19:39 -0400

    Changed focus in correctness. But I'm not in a correct state yet.

--- services/api/scott.ts
+++ services/api/scott.ts
@@ -1,32 +1,33 @@
 import axios from "axios"
 import 'reflect-metadata'
-import {IDiscussionService} from "../../utils/interfaces"
 import postModel from "../../models/postModel"
 import { injectable } from "inversify"
 import { dict_constructor } from "../../utils/decorators"
+import { IDiscussionService } from "../../utils/interfaces"
 
 
 @injectable()
 export default class scott_api_service implements IDiscussionService {
-    get_discussion_by_filter: { trending: Function; new: Function; promoted: Function; hot: Function }
+    get_discussion_by_filter: { trending: postModel[]; new: postModel[]; promoted: postModel[]; hot: postModel[] }
     discussion : postModel[]        
     urlAPI: string
-    get_discussions(filter: string, query: string): Promise<any> {    
-        
+    async get_discussions(filter: string, query: string): Promise<any> {            
         return axios.get(this.urlAPI + '/get_discussions_by_' + filter + '?' + query )                
-    }
-
-    
-    public get_discussion_by_trending(token = 'BLQ' , limit = '20', tag = 'bloque64') 
+    }    
+    async connect_to_api_for_get_discussion_by_trending(token = 'BLQ' , limit = '20', tag = 'bloque64') 
     {
         const params = ['token=' +token, 'limit=' + limit, 'tag=' + tag];
-        const query =  params.join('&') + Array.prototype.slice.call(arguments, 3).join('&');
-        
-        this.get_discussions('trending', query).then( response => this.discussion = dict_constructor(response.data) ).catch( (err) => console.log(err.message) );        
+        const query =  params.join('&') + Array.prototype.slice.call(arguments, 3).join('&');        
+        await this.get_discussions('trending', query).then( response => this.discussion = dict_constructor(response.data) ).catch( (err) => console.log(Error (err.message) ) );        
     }
     /**
      *
      */    
+    public get get_discussion_by_trending() : postModel[] {
+        this.connect_to_api_for_get_discussion_by_trending()
+        return this.discussion
+    }
+    
     constructor() {
         this.urlAPI = 'https://scot-api.steem-engine.com/';
         this.get_discussion_by_filter = { 
@@ -38,4 +39,6 @@ export default class scott_api_service implements IDiscussionService {
          this.discussion = Array<postModel>()       
     }        
 }
+
+
 
