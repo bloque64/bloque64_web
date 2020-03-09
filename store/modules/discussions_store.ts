45dfb63db39742bcb5af7baad5361a08ed9abd42
DiazRock <corolariodiaz@gmail.com>
2020-03-09 01:24:01 -0400

    Included discussions_store in modules directory because of a nuxt exception

---
+++ store/modules/discussions_store.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata'
+import container from '../../utils/inversify.config'
+import { Module, VuexModule, Mutation, Action, getModule } from 'vuex-module-decorators'
+import postModel from '../../models/postModel'
+import { IDiscussionService, IDiscussionStore, TYPES } from '../../utils/interfaces'
+import {Store} from "../index"
+// import { injectable } from "inversify"
+
+const service: IDiscussionService = container.get<IDiscussionService>(TYPES.IDiscussionService)
+@Module({
+    name: 'discussion_store',
+    stateFactory: true,
+    store : Store,
+    dynamic: true,
+    namespaced: true,
+  })
+export class discussion_store extends VuexModule implements IDiscussionStore
+{
+    discussionList: postModel[] = []
+    @Mutation
+    setList ( discussionList : postModel[])
+    {
+        this.discussionList =  discussionList;
+    }
+    @Action ({commit : 'setList'})
+    async get_by_trending () {
+        return service.get_discussion_by_filter['trending']        
+    }    
+    get discussion() : postModel[] {
+        return this.discussionList
+    }
+}
+
+export default getModule(discussion_store)
