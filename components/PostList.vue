bba03b103f7dcc6d7f70f2063f56c710876af26c
DiazRock <corolariodiaz@gmail.com>
2020-03-10 00:32:25 -0400

    Show the store getter, and after a refresh, it desapear. Things are not good, but are better than yesterday.

--- components/PostList.vue
+++ components/PostList.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
-    <h1>Category</h1>
-
-    <div v-for="post in posts" :key="post.authorperm">
-      <!-- {{ post.authorperm }} /> -->
-      <PostSummary />
-    </div>
+    <h1></h1>
+    {{ posts }}
+    <!-- <div v-for="post in posts" :key="post.authorperm">
+       {{ post.authorperm }} />
+       <PostSummary />
+    </div> -->
   </div>
 </template>
 
 <script lang = "ts">
-
+import 'reflect-metadata'
 import { Vue, Component } from 'vue-property-decorator'
 import discussionStore from '../store/modules/discussions_store'
 import PostSummary from '~/components/PostSummary.vue'
-import postModel from '~/models/postModel'
+// import postModel from '~/models/postModel'
 
 @Component({
   components: {
     PostSummary
-  },
-  middleware: 'discussion_load'
+  }
 })
 class PostList extends Vue {
-  storeService = discussionStore
-  posts : postModel[] = []
-  setDiscussionPost () {
-    this.posts = this.storeService.context.getters.discussion
+  get posts () {
+    // eslint-disable-next-line no-console
+    console.log(discussionStore.discussionList)
+    // eslint-disable-next-line no-console
+    console.log('In the momment when executes the getter in PostList.vue')
+    return discussionStore.discussionList
   }
 }
 
@@ -59,11 +60,7 @@ class PostList extends Vue {
   }
 
   /* created () {
-     axios
-      .get('https://scot-api.steem-engine.com/get_discussions_by_trending?token=BLQ&limit=20&tag=bloque64')
-      .then((response) => {
-        this.posts = response.data
-      })
+     axios.get('https://scot-api.steem-engine.com/get_discussions_by_trending?token=BLQ&limit=20&tag=bloque64') .then((response) => { this.posts = response.data })
       .catch((error) => {
         console.log(error)
         this.errored = true
