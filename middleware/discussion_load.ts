613bb6af3f1df619bc0fbe481bcddd51f232a3cd
DiazRock <corolariodiaz@gmail.com>
2020-03-10 00:33:43 -0400

    added the await to the middleware load discussion function

--- middleware/discussion_load.ts
+++ middleware/discussion_load.ts
@@ -3,7 +3,8 @@ import { Middleware } from '@nuxt/types'
 import discussionStore from '../store/modules/discussions_store'
 
 const discussion_load: Middleware = async ({}) => {
-    discussionStore.get_by_trending()    
+    console.log("Inside middleware")
+    await discussionStore.get_by_trending()    
 }
 
 export default discussion_load
