1111d435000b06e08cb5f8bcc80ebcfbe31c69f2
DiazRock <corolariodiaz@gmail.com>
2020-03-12 15:21:29 -0400

    It wokrs in ssr, but in csr, the states goes away. Perhaps its related to the VDOM warning

--- middleware/discussion_load.ts
+++ middleware/discussion_load.ts
@@ -3,7 +3,6 @@ import { Middleware } from '@nuxt/types'
 import discussionStore from '../store/modules/discussions_store'
 
 const discussion_load: Middleware = async ({}) => {
-    console.log("Inside middleware")
     await discussionStore.get_by_trending()    
 }
 
