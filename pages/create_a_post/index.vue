ccf6c57e5907ed52ed5cc1243109d2f8da09f1d4
DiazRock <corolariodiaz@gmail.com>
2020-04-23 11:15:52 -0400

    The pages for a post creation

---
+++ pages/create_a_post/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang = "ts">
+import 'reflect-metadata'
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+})
+class createPost extends Vue {
+
+}
+export default createPost
+</script>
+
+<style>
+
+</style>
---
+++ pages/create_a_post/write_bulletin.vue
@@ -0,0 +1,23 @@
+<template>
+  <editor />
+</template>
+
+<script lang = "ts">
+import { Vue, Component } from 'vue-property-decorator'
+import editor from '~/components/bulletin_editor.vue'
+
+@Component({
+  components: {
+    editor
+  }
+})
+class BulletinWritter extends Vue {
+    content :string = ''
+    options = {}
+}
+export default BulletinWritter
+</script>
+
+<style>
+
+</style>
