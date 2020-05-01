c332aae206ab7409498925d4d7a6a7095e3faac6
DiazRock <corolariodiaz@gmail.com>
2020-05-01 14:04:26 -0400

    I started to play with the TopBar

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="TopBar">
-    17.07.2019
-  </div>
+  <b-navbar toggleable="md" variant="light">
+    <b-container>
+      <b-navbar-toggle target="navbar-collapse" />
+      <b-collapse id="nav_collapse" is-nav>
+        <b-col>
+          <b-nav-text>
+            Aquí tamos
+          </b-nav-text>
+        </b-col>
+        <b-col>
+          <b-nav-item-dropdown right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-col>
+        <b-col>
+          <b-navbar-nav>
+            <b-nav-item> Cortarse las venas </b-nav-item>
+            <b-nav-item> Tirarse de un puente </b-nav-item>
+          </b-navbar-nav>
+        </b-col>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </template>
 
 <script>
