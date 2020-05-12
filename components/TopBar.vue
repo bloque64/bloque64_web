9c0aa01794f21f7198b00269097690175e3998c4
DiazRock <corolariodiaz@gmail.com>
2020-05-12 15:32:49 -0400

    I got some fancy icons

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,40 +1,48 @@
 <template>
-  <b-navbar toggleable="md" variant="light">
-    <b-container>
-      <b-navbar-toggle target="navbar-collapse" />
-      <b-collapse id="nav_collapse" is-nav>
-        <b-col>
-          <b-nav-text>
-            Aquí tamos
+  <div class="top-bar">
+    <b-navbar toggleable="md" variant="light">
+      <b-container fluid>
+        <b-col class="ml-n4">
+          <b-nav-text style="letter-spacing: -0.113em;">
+            {{ peek_date() }}
           </b-nav-text>
         </b-col>
         <b-col>
-          <b-nav-item-dropdown right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <!-- <b-nav-form>
+            Cosas se verán mateo
+          </b-nav-form> -->
         </b-col>
-        <b-col>
-          <b-navbar-nav>
-            <b-nav-item> Cortarse las venas </b-nav-item>
-            <b-nav-item> Tirarse de un puente </b-nav-item>
-          </b-navbar-nav>
+        <b-col offset="8" align-self="end">
+          <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'pen-square' }" />
+          <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'user-circle'}" />
+          <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'list' }" />
         </b-col>
-      </b-collapse>
-    </b-container>
-  </b-navbar>
+      </b-container>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'TopBar',
   props: {
     msg: {
       type: String,
       default: 'No existe el éxito, sino la belleza. Nosotros la buscamos, y la compartimos con usted'
+    }
+  },
+  data () {
+    return {
+      value: '',
+      context: null
+    }
+  },
+  methods: {
+    peek_date () {
+      const dt = new Date()
+      const dtf = new Intl.DateTimeFormat('sp', { year: 'numeric', month: 'short', day: '2-digit' })
+      const [{ value: mo },, { value: da },, { value: ye }] = dtf.formatToParts(dt)
+      return `${da} ${mo} ${ye}`
     }
   }
 }
@@ -46,15 +54,14 @@ export default {
 .top-bar {
 
   margin: 0px;
-  padding: 10px;
+  padding: 0px;
   overflow: hidden;
   background-color: white;
-  font: normal 14px American Typewriter;
-  border-style: dotted;
+  font: normal 20px American Typewriter;
+  border-style: solid;
   border-left: 0px;
   border-right: 0px;
   border-top: 0px;
-  border-botton: 1px;
   border-width: 1px;
 
 }
