b961a70eaca5c1c564e0999d928d19687120875a
DiazRock <corolariodiaz@gmail.com>
2020-07-26 13:41:13 -0400

    Other cahnges

--- store/modules/detail_post_store.ts
+++ store/modules/detail_post_store.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
-import container from '../../utils/inversify.config'
 import { VuexModule, Mutation, Action, getModule, Module } from 'vuex-module-decorators'
+import container from '../../utils/inversify.config'
 import postModel from '../../models/postModel'
 import { IDetailPostService, TYPES } from '../../utils/interfaces'
 import { Store } from '@/store'
@@ -9,31 +9,30 @@ const service: IDetailPostService = container.get<IDetailPostService>(TYPES.IDet
 /* console.log(service)
 console.log('Aquí muy casual en el detail post store') */
 @Module({
-    name: 'detail_post_store',
-    stateFactory: true,
-    store: Store,
-    dynamic: true,
-    namespaced: true,
+  name: 'detail_post_store',
+  stateFactory: true,
+  store: Store,
+  namespaced: true,
+  dynamic: true
 })
-class detail_post_store extends VuexModule{
+class detail_post_store extends VuexModule {
     post_info: postModel | undefined
     @Mutation
-    set_post_values(post: postModel){
-        console.log('Muerto de risa en el set_post_mutation')
-        console.log(post.body)
-        this.post_info = post
+    set_post_values (post: postModel) {
+      this.post_info = post
     }
-    @Action({ commit : 'set_post_values' })
-    async get_aditional_post_details (post: postModel){
-        console.log(' En el action get_aditional del store before')
-        await service.get_detail_post_params(post).then(result => post = result)
-        console.log(' En el action get_aditional del store before ')
-        return post
+
+    @Action({ commit: 'set_post_values' })
+    async get_aditional_post_details (author: string, permlink : string) {
+      console.log(' en el get_aditional_post_details antes de la llamada')
+      const result = await service.get_detail_post(author, permlink)
+      .catch( (err : string) => console.log('error chama, ', err) )
+      console.log(result, ' en el get_aditional_post_details')
+      return result
     }
-    get post(): postModel | undefined{
-        console.log('En el get post del store')
-        console.log(this.post_info)
-        return this.post_info
+
+    get post (): postModel | undefined {
+      return this.post_info
     }
 }
 
