8d5897ec6804706af8d4a6d448372859af8a0ff2
DiazRock <corolariodiaz@gmail.com>
2020-07-26 12:30:16 -0400

    Added new interfaces

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -2,7 +2,7 @@
 import postModel from '../models/postModel'
 
 export interface IDiscussionService {
-    
+
     get_discussion_by_filter: {
         trending: Promise<postModel[]>,
         new: Promise<postModel[]>,
@@ -10,17 +10,51 @@ export interface IDiscussionService {
         hot: Promise<postModel[]>
     };
     get_discussions (filter: string, query: string): Promise<any>
-    
+
 };
 
 export interface IDetailPostService {
-    get_detail_post (author:string, permlink: string) : any
-    get_detail_post_params (post: postModel) : Promise <postModel>
+    get_detail_post (author:string, permlink: string) : any,
+}
+
+export interface IVoteService {
+    vote(voter: string,
+        votedAuthor: string,
+        permlink: string,
+        weight: string) : any
+}
+
+export interface ILogger {
+    login(dict : {userName : string, access_token : string}) : void,
+    logout() : any
+}
+
+export interface IPostService {
+    post(parentAuthor: string,
+        parentPermlink: string,
+        author: string,
+        permlink: string,
+        title: string,
+        body: string,
+        jsonMetadata: any) : any
+}
+
+export interface IUserDetails{
+    userProfile() : Promise<any>
+}
+
+export interface ILogginDispatcher {
+    dispatchLogin(callbackURL: string) : any
 }
 
 export const TYPES =
 {
-    IDiscussionService: Symbol("IDiscussionService"),
-    IDetailPostService: Symbol("IDetailPostService"),
-    IDiscussionStore: Symbol("IDiscussionStore")
+  IDiscussionService: Symbol('IDiscussionService'),
+  IDetailPostService: Symbol('IDetailPostService'),
+  IDiscussionStore: Symbol('IDiscussionStore'),
+  IVoteService: Symbol('IVoteService'),
+  ILogger: Symbol('ILogger'),
+  IPostService: Symbol('IPostService'),
+  IUserDetails: Symbol('IUserDetails'),
+  ILogginDispatcher: Symbol('ILogginDispatcher')
 }
--- utils/inversify.config.ts
+++ utils/inversify.config.ts
@@ -1,14 +1,27 @@
 import 'reflect-metadata'
-import {Container} from "inversify"
-import {TYPES, IDiscussionService, IDetailPostService} from "../utils/interfaces"
-import scott_api from "../services/api/scott"
-import hive_api from "../services/api/hive"
+import { Container } from 'inversify'
+import {
+  TYPES,
+  IDiscussionService,
+  IDetailPostService,
+  ILogger,
+  IPostService,
+  IVoteService,
+  IUserDetails,
+  ILogginDispatcher
+} from '../utils/interfaces'
+import scottApi from '../services/api/scott'
+import hiveApi from '../services/api/hive'
+import hivesignerService from '~/services/api/hivesigner'
 
-let container = new Container()
-container.bind<IDiscussionService>(TYPES.IDiscussionService).to(scott_api).inTransientScope()
-container.bind<IDetailPostService>(TYPES.IDetailPostService).to(hive_api)
+const container = new Container()
+const hiveConnector = new hivesignerService()
+container.bind<IDiscussionService>(TYPES.IDiscussionService).to(scottApi).inTransientScope()
+container.bind<IDetailPostService>(TYPES.IDetailPostService).to(hiveApi)
+container.bind<ILogger>(TYPES.ILogger).toConstantValue(hiveConnector)
+container.bind<IPostService>(TYPES.IPostService).toConstantValue(hiveConnector)
+container.bind<IVoteService>(TYPES.IVoteService).toConstantValue(hiveConnector)
+container.bind<IUserDetails>(TYPES.IUserDetails).toConstantValue(hiveConnector)
+container.bind<ILogginDispatcher>(TYPES.ILogginDispatcher).toConstantValue(hiveConnector)
+
 export default container
-
-
-
-
