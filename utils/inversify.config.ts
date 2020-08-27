e342e285bc738e338e19a0c3cff7ebe6b48f8f3b
DiazRock <corolariodiaz@gmail.com>
2020-08-27 13:04:29 -0400

    A new container

--- utils/inversify.config.ts
+++ utils/inversify.config.ts
@@ -8,16 +8,22 @@ import {
   IPostService,
   IVoteService,
   IUserDetails,
-  ILogginDispatcher
-} from '../utils/interfaces'
-import scottApi from '../services/api/scott'
-import hiveApi from '../services/api/hive'
+  ILogginDispatcher,
+  IParserSummaryText,
+  IParserFullText,
+  IRenderCleanText,
+  IStringify
+} from '~/utils/interfaces'
+import scottApi from '~/services/api/scott'
+import compoundHiveAndScott from '~/services/api/compoundHiveAndScott'
 import hivesignerService from '~/services/api/hivesigner'
+import { parserSummary } from "~/utils/postVisualizeTools/markdownParser";
+
 
 const container = new Container()
 const hiveConnector = new hivesignerService()
 container.bind<IDiscussionService>(TYPES.IDiscussionService).to(scottApi).inTransientScope()
-container.bind<IDetailPostService>(TYPES.IDetailPostService).to(hiveApi)
+container.bind<IDetailPostService>(TYPES.IDetailPostService).to(compoundHiveAndScott)
 container.bind<ILogger>(TYPES.ILogger).toConstantValue(hiveConnector)
 container.bind<IPostService>(TYPES.IPostService).toConstantValue(hiveConnector)
 container.bind<IVoteService>(TYPES.IVoteService).toConstantValue(hiveConnector)
@@ -25,3 +31,10 @@ container.bind<IUserDetails>(TYPES.IUserDetails).toConstantValue(hiveConnector)
 container.bind<ILogginDispatcher>(TYPES.ILogginDispatcher).toConstantValue(hiveConnector)
 
 export default container
+
+export const componentsContainer = new Container()
+const p = new parserSummary()
+componentsContainer.bind<IParserSummaryText>(TYPES.IParserSummaryText).toConstantValue(p)
+componentsContainer.bind<IParserFullText>(TYPES.IParserFullText).toConstantValue(p)
+componentsContainer.bind<IRenderCleanText>(TYPES.IRenderCleanText).toConstantValue(p)
+componentsContainer.bind<IStringify>(TYPES.IStringify).toConstantValue(p)
