f32ab4a320fa017768df270bce810ea28e715d73
DiazRock <corolariodiaz@gmail.com>
2020-08-27 12:57:12 -0400

    Changed the post_view for author_view

---
+++ pages/_author/_permlink.vue
@@ -0,0 +1,47 @@
+<template>
+  <div> 
+    <div v-if="state === 'SUCCESS'"> 
+      <postDetail />
+    </div>
+    <div v-else-if="state === 'FAIL'"> 
+      ERROR
+    </div>
+    <div v-else-if="state === 'PENDING'"> 
+      <loading-component />
+    </div>
+  </div> 
+  
+</template>
+
+<script lang = "ts">
+import 'reflect-metadata'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import detailPostStore from '~/store/modules/detail_post_store'
+import postDetail from '~/components/organisms/postDetail.vue'
+import loadingComponent from '~/components/molecules/loadingComponent.vue'
+import postModel from '~/models/postModel'
+
+@Component({
+  components: {
+    postDetail,
+    loadingComponent
+  }
+})
+class postView extends Vue {
+  state : 'SUCCESS' | 'FAIL' | 'PENDING' = 'PENDING'
+
+  created () {
+    detailPostStore.get_aditional_post_details({
+        author: this.$route.params.author.
+        slice(1, this.$route.params.author.length),
+        permlink: this.$route.params.permlink})
+        .then(() => this.state= 'SUCCESS')
+        .catch((err:any) => {console.log(err); this.state= 'FAIL'})
+  }
+}
+export default postView
+</script>
+
+<style>
+
+</style>
