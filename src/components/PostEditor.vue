64fbe343aefcc5312310d4b199578f3113e9a74f
Pablo <pgarcgo@bloque64.com>
2020-10-20 20:25:04 +0200

    Advancing in News Editor

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -15,26 +15,29 @@ Body - Cuerpo
 
 
 
-  <div class="edit_post">
-       <div class="edit_news">
 
-        <form id="posteditor_form">
+       <div class="editor">
+        
+        <form class ="news_form" id="posteditor_form" @submit.prevent="processForm">
                 <label for="context">Epígrafe:</label><br>
-                <textarea name="context" cols="60" rows="3"></textarea><br>
+                <textarea class = "formelements" width=10px name="context" rows="3" v-model="context"></textarea><br>
                 <label for="healine">Titular:</label><br>
-                <textarea name="context" cols="60" rows="2"></textarea><br>
+                <textarea class = "formelements" name="headline" cols="60" rows="2"></textarea><br>
                 <label for="subtitle">Subtítulo:</label><br>
-                <textarea name="subtitle" cols="60" rows="2"></textarea><br>
+                <textarea class = "formelements" name="subtitle" cols="60" rows="2"></textarea><br>
                 <label for="byline">Autor:</label><br>
-                <input name="byline" maxlength="70" size="70"><br><br>
+                <input class = "formelements" name="byline" maxlength="70" size="70"><br><br>
                 <label for="lead">Entradilla:</label><br>
-                <textarea name="lead" cols="60" rows="8"></textarea><br>
+                <textarea class = "formelements" name="lead" cols="60" rows="8"></textarea><br>
                 <label for="body">Cuerpo:</label><br>
-                <textarea name="cuerpo" cols="60" rows="20"></textarea><br>
+                <textarea class = "formelements" name="cuerpo" cols="60" rows="20"></textarea><br>
+                <br>
+                <button type="button" id="store">Guardar</button>
+                <button type="submit" id="publish">Publicar</button>
             </form>
   
         </div>
-  </div>
+
 </template>
 
 <script>
@@ -46,12 +49,71 @@ Body - Cuerpo
         data() {
 
             return {
-                title: "title"                    
+                context: "" 
+
             }
+        },
+
+        methods: {
+
+
+            processForm: function() {
+
+                console.log({ context: this.context});
+                //alert(this.context);
+            }
+
+
         }
     }
 
 </script>
 
 <style>
+
+.editor {
+
+  margin: 0px;
+  padding: 20px;
+  font: normal 14px American Typewriter;
+  border-left: 1px;
+  border-right: 1px;
+  border-top: 1px;
+  border-botton: 10px;
+  border-width: 1px;
+  width: auto;
+  margin: auto auto;
+  border-style: 1px solid black;
+  border-top: 1px dotted black;
+
+
+}
+
+.form_title {
+
+
+    width: 700px;
+    margin: 0px;
+    border: 1px dotted white;
+
+}
+
+.news_form {
+
+    width: 700px;
+    background-color: rgb(205, 205, 205);
+    padding: 20px;
+    border: 1px dotted black;
+    margin: 0 auto;
+
+
+}  
+
+.formelements {
+
+
+    width: 100%;
+    
+}
+
 </style>
